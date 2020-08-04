68d711a0bf31b13e4603fc65e9cd3766e8987ad0
Edartista <f.vinicius.edart@gmail.com>
2020-08-04 19:46:44 -0300

    Criando a classe Departamento

---
+++ src/Entidades/Departamento.java
@@ -0,0 +1,63 @@
+package Entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departamento implements Serializable {
+    
+    private Integer id;
+    private String nome;
+    
+    public Departamento(){
+    }
+
+    public Departamento(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Departamento other = (Departamento) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String toString(){
+        return String.format("Dados do departamento:\nNome: %s \nID: %d", nome, id);
+    }
+}
