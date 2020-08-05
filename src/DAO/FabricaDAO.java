e0b35b48623fd7e077251dd39b0c279fa15ae19a
Edartista <f.vinicius.edart@gmail.com>
2020-08-04 23:44:17 -0300

    Criando as implementações VendedorDAOJDBC e FabricaDAO

---
+++ src/DAO/FabricaDAO.java
@@ -0,0 +1,9 @@
+package DAO;
+import DAO.Impl.VendedorDAOImplJDBC;
+public class FabricaDAO {
+    
+    public static VendedorDAO criarVendedorDAO(){
+        return new Vendedor
+    }
+    
+}
---
+++ src/DAO/Impl/VendedorDAOJDBC.java
@@ -0,0 +1,36 @@
+package DAO.Impl;
+
+import DAO.VendedorDAO;
+import Entidades.Vendedor;
+import java.util.List;
+
+public class VendedorDAOJDBC implements VendedorDAO{
+
+    @Override
+    public void inserir(Vendedor obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void atualizar(Vendedor obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletarPorId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Vendedor procurarPorId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Vendedor> encontrarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
